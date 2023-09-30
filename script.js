67de39305f5614d3a03db3971d13401879b18f3e
alfarruq <mengboyevfarrux01@gmail.com>
2023-09-30 12:03:02 +0500

    added login code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// for githup test
+let your_name  = document.getElementById('your_name')
+let your_lastname  = document.getElementById('your_lastname')
+
+let name  = prompt('your name');
+let lastname = prompt('your lastname');
+
+
+your_name.innerText = name;
+your_lastname.innerText = lastname;
